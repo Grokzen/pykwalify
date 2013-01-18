ee3b30cfbfa5b689c23a0ab33ff2a2fda69e0dc4
Grokzen <Grokzen@gmail.com>
2013-01-18 19:14:01 +0100

    Added more tests to TestCore. Still some broken functionality.

--- tests/lib/testcore.py
+++ tests/lib/testcore.py
@@ -34,8 +34,63 @@ class TestCore(TestHelper):
         with self.assertRaises(Exception):
             c.run_core()
 
+        a = [1, 2, 3, True, False]
+        b = {"type": "seq", "sequence": [ {"type": "int"} ] }
+        c = Core(source_data = a, schema_data = b)
+        c.run_core()
+
+        a = [True, False]
+        b = {"type": "seq", "sequence": [ {"type": "bool"} ] }
+        c = Core(source_data = a, schema_data = b)
+        c.run_core()
+
+        a = [True, False]
+        b = {"type": "seq", "sequence": [ {"type": "str"} ] }
+        c = Core(source_data = a, schema_data = b)
+        with self.assertRaises(Exception):
+            c.run_core()
+
+        a = [True, False, 1]
+        b = {"type": "seq", "sequence": [ {"type": "bool"} ] }
+        c = Core(source_data = a, schema_data = b)
+        with self.assertRaises(Exception):
+            c.run_core()
+
         a = ["foo", "bar", "foobar"]
         b = {"type": "seq", "sequence": [ {"type": "str", "length": {"max": 5, "min": 1} } ] }
         c = Core(source_data = a, schema_data = b)
         with self.assertRaises(Exception):
             c.run_core()
+
+        a = {"name": "foo", "email": "foo@mail.com", "age": 20, "birth": "1985-01-01"}
+        b = {"type": "map", "mapping": {"name": {"type": "str", "required": True}, "email": {"type": "str", "pattern": ".+@.+"}, "age": {"type": "int"}, "birth": {"type": "str"} } }
+        c = Core(source_data = a, schema_data = b)
+        c.run_core()
+
+        a = {"name": "foo", "email": "foo(at)mail.com", "age": "twnty", "birth": "Jun 01, 1985"}
+        c = Core(source_data = a, schema_data = b)
+        with self.assertRaises(Exception):
+            c.run_core()
+
+        a = [ {"name": "foo", "email": "foo@mail.com"}, {"name": "bar", "email": "bar@mail.net"}, {"name": "baz", "email": "baz@mail.org"} ]
+        b = {"type": "seq", "sequence": [ {"type": "map", "mapping": {"name": {"type": "str"}, "email": {"type": "str"} } } ] }
+        c = Core(source_data = a, schema_data = b)
+        c.run_core()
+
+        # TODO: (line 3) [/1] key 'name:' is required. Not working yet
+        a = [ {"name": "foo", "email": "foo@mail.com"}, {"naem": "bar", "email": "bar@mail.net"}, {"name": "baz", "mail": "baz@mail.org"} ]
+        c = Core(source_data = a, schema_data = b)
+        with self.assertRaises(Exception):
+            c.run_core()
+
+        a = {"company": "Kuwata lab.", "email": "webmaster@kuwata-lab.com", "employees": [{"code": 101, "name": "foo", "email": "foo@kuwata-lab.com"}, {"code": 102, "name": "bar", "email": "bar@kuwata-lab.com"}] }
+        b = {"type": "map", "mapping": {"company": {"type": "str", "required": True}, "email": {"type": "str"}, "employees": {"type": "seq", "sequence": [ {"type": "map", "mapping": {"code": {"type": "int", "required": True}, "name": {"type": "str", "required": True}, "email": {"type": "str"} } } ] } } }
+        c = Core(source_data = a, schema_data = b)
+        c.run_core()
+
+        # TODO: - (line 9) [/employees/1/mail] key 'mail:' is undefined. Not working yet
+        a = {"company": "Kuwata Lab.", "email": "Webmaster@kuwata-lab.com", "employees": [ {"code": "A101", "name": "foo", "email": "foo@kuwta-lab.com"}, {"code": 102, "name": "bar", "email": "bar@kuwata-lab.com"} ] }
+        c = Core(source_data = a, schema_data = b)
+        with self.assertRaises(Exception):
+            c.run_core()
+
