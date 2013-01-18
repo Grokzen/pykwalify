bfe5c9d9df74d0cd4fdcaf9cf8f5d03d1c5073d7
Grokzen <Grokzen@gmail.com>
2013-01-18 21:55:37 +0100

    Added comments to all Core tests shortly describe what it does. Restructured the data to make it easier to read. Fixed the 2 broken tests. All Core test samples now work as intended.

--- tests/lib/testcore.py
+++ tests/lib/testcore.py
@@ -34,63 +34,89 @@ class TestCore(TestHelper):
         with self.assertRaises(Exception):
             c.run_core()
 
+        # Test sequence where the only valid items is integers
         a = [1, 2, 3, True, False]
         b = {"type": "seq", "sequence": [ {"type": "int"} ] }
         c = Core(source_data = a, schema_data = b)
         c.run_core()
 
+        # Test sequence with only booleans
         a = [True, False]
         b = {"type": "seq", "sequence": [ {"type": "bool"} ] }
         c = Core(source_data = a, schema_data = b)
         c.run_core()
 
+        # Test sequence with defined string content type but data only has booleans
         a = [True, False]
         b = {"type": "seq", "sequence": [ {"type": "str"} ] }
         c = Core(source_data = a, schema_data = b)
         with self.assertRaises(Exception):
             c.run_core()
 
+        # Test sequence with defined booleans but with one integer
         a = [True, False, 1]
         b = {"type": "seq", "sequence": [ {"type": "bool"} ] }
         c = Core(source_data = a, schema_data = b)
         with self.assertRaises(Exception):
             c.run_core()
 
+        # Test sequence with strings and and lenght on each string
         a = ["foo", "bar", "foobar"]
-        b = {"type": "seq", "sequence": [ {"type": "str", "length": {"max": 5, "min": 1} } ] }
+        b = {"type": "seq", "sequence": [ {"type": "str", 
+                                           "length": {"max": 5, "min": 1} } ] }
         c = Core(source_data = a, schema_data = b)
         with self.assertRaises(Exception):
             c.run_core()
 
+        # Test mapping with different types of data and some extra conditions
         a = {"name": "foo", "email": "foo@mail.com", "age": 20, "birth": "1985-01-01"}
-        b = {"type": "map", "mapping": {"name": {"type": "str", "required": True}, "email": {"type": "str", "pattern": ".+@.+"}, "age": {"type": "int"}, "birth": {"type": "str"} } }
+        b = {"type": "map", "mapping": {"name":  {"type": "str", "required": True}, 
+                                        "email": {"type": "str", "pattern": ".+@.+"}, 
+                                        "age":   {"type": "int"}, 
+                                        "birth": {"type": "str"} } }
         c = Core(source_data = a, schema_data = b)
         c.run_core()
 
-        a = {"name": "foo", "email": "foo(at)mail.com", "age": "twnty", "birth": "Jun 01, 1985"}
+        # Test mapping that do not work
+        a = {"name": "foo", 
+             "email": "foo(at)mail.com", 
+             "age": "twnty", 
+             "birth": "Jun 01, 1985"}
         c = Core(source_data = a, schema_data = b)
         with self.assertRaises(Exception):
             c.run_core()
 
-        a = [ {"name": "foo", "email": "foo@mail.com"}, {"name": "bar", "email": "bar@mail.net"}, {"name": "baz", "email": "baz@mail.org"} ]
-        b = {"type": "seq", "sequence": [ {"type": "map", "mapping": {"name": {"type": "str"}, "email": {"type": "str"} } } ] }
+        # Test sequence with mapping with valid mapping
+        a = [ {"name": "foo", "email": "foo@mail.com"}, 
+              {"name": "bar", "email": "bar@mail.net"}, 
+              {"name": "baz", "email": "baz@mail.org"} ]
+        b = {"type": "seq", "sequence": [ {"type": "map", "mapping": {"name":  {"type": "str", "required": True}, 
+                                                                      "email": {"type": "str"} } } ] }
         c = Core(source_data = a, schema_data = b)
         c.run_core()
 
-        # TODO: (line 3) [/1] key 'name:' is required. Not working yet
-        a = [ {"name": "foo", "email": "foo@mail.com"}, {"naem": "bar", "email": "bar@mail.net"}, {"name": "baz", "mail": "baz@mail.org"} ]
+        # Test sequence with mapping with missing required key
+        a = [ {"name": "foo", "email": "foo@mail.com"}, 
+              {"naem": "bar", "email": "bar@mail.net"}, 
+              {"name": "baz", "mail": "baz@mail.org"} ]
         c = Core(source_data = a, schema_data = b)
         with self.assertRaises(Exception):
             c.run_core()
 
-        a = {"company": "Kuwata lab.", "email": "webmaster@kuwata-lab.com", "employees": [{"code": 101, "name": "foo", "email": "foo@kuwata-lab.com"}, {"code": 102, "name": "bar", "email": "bar@kuwata-lab.com"}] }
-        b = {"type": "map", "mapping": {"company": {"type": "str", "required": True}, "email": {"type": "str"}, "employees": {"type": "seq", "sequence": [ {"type": "map", "mapping": {"code": {"type": "int", "required": True}, "name": {"type": "str", "required": True}, "email": {"type": "str"} } } ] } } }
+        # Test mapping with sequence with mapping and valid data
+        a = {"company": "Kuwata lab.", "email": "webmaster@kuwata-lab.com", "employees": [ {"code": 101, "name": "foo", "email": "foo@kuwata-lab.com"}, 
+                                                                                           {"code": 102, "name": "bar", "email": "bar@kuwata-lab.com"} ] }
+        b = {"type": "map", "mapping": {"company": {"type": "str", "required": True}, 
+                                        "email": {"type": "str"}, 
+                                        "employees": {"type": "seq", "sequence": [ {"type": "map", "mapping": {"code": {"type": "int", "required": True}, 
+                                                                                                               "name": {"type": "str", "required": True}, 
+                                                                                                               "email": {"type": "str"} } } ] } } }
         c = Core(source_data = a, schema_data = b)
         c.run_core()
 
-        # TODO: - (line 9) [/employees/1/mail] key 'mail:' is undefined. Not working yet
-        a = {"company": "Kuwata Lab.", "email": "Webmaster@kuwata-lab.com", "employees": [ {"code": "A101", "name": "foo", "email": "foo@kuwta-lab.com"}, {"code": 102, "name": "bar", "email": "bar@kuwata-lab.com"} ] }
+        # Test mapping with sequence with mapping and invalid data
+        a = {"company": "Kuwata Lab.", "email": "Webmaster@kuwata-lab.com", "employees": [ {"code": "A101", "name": "foo", "email": "foo@kuwta-lab.com"}, 
+                                                                                           {"code": 102,    "name": "bar", "mail": "bar@kuwata-lab.com"} ] }
         c = Core(source_data = a, schema_data = b)
         with self.assertRaises(Exception):
             c.run_core()
-
