2b0b6bc3b8a6168284995f5e2baa4993a572f441
Grokzen <Grokzen@gmail.com>
2013-01-26 01:19:59 +0100

    Added some basic DataMode tests to test all basic types both working and invalid types. Streamlined all tests in testCore() to take less code space :]

--- tests/lib/testcore.py
+++ tests/lib/testcore.py
@@ -14,126 +14,124 @@ class TestCore(TestHelper):
         return gettestcwd("tests", "files", *args)
 
     def testCoreDataMode(self):
-        c = Core(source_data = 3.14159, schema_data = {"type": "number"} )
-        c.run_core()
+        Core(source_data = 3.14159,  schema_data = {"type": "number"} ).run_core()
+        Core(source_data = 3.14159,  schema_data = {"type": "float"} ).run_core()
+        Core(source_data = 3,        schema_data = {"type": "int"} ).run_core()
+        Core(source_data = True,     schema_data = {"type": "bool"} ).run_core()
+        Core(source_data = "foobar", schema_data = {"type": "str"} ).run_core()
+        Core(source_data = "foobar", schema_data = {"type": "text"} ).run_core()
+        Core(source_data = "foobar", schema_data = {"type": "any"} ).run_core()
+
+        with self.assertRaises(Exception):
+            Core(source_data = "abc",  schema_data = {"type": "number"} ).run_core()
+
+        with self.assertRaises(Exception):
+            Core(source_data = 3, schema_data = {"type": "float"} ).run_core()
+
+        with self.assertRaises(Exception):
+            Core(source_data = 3.14159, schema_data = {"type": "int"} ).run_core()
+
+        with self.assertRaises(Exception):
+            Core(source_data = 1337, schema_data = {"type": "bool"} ).run_core()
+
+        with self.assertRaises(Exception):
+            Core(source_data = 1, schema_data = {"type": "str"} ).run_core()
+
+        with self.assertRaises(Exception):
+            Core(source_data = True, schema_data = {"type": "text"} ).run_core()
+
+        with self.assertRaises(Exception):
+            Core(source_data = dict, schema_data = {"type": "any"} ).run_core()
 
     def testCore(self):
         # Test sequence with only string values
-        c = Core(source_file = self.f("1a.yaml"), schema_file = self.f("1b.yaml") )
-        c.run_core()
+        Core(source_file = self.f("1a.yaml"), schema_file = self.f("1b.yaml") ).run_core()
 
         # Test sequence with defined string content type but data only has integers
-        c = Core(source_file = self.f("2a.yaml"), schema_file = self.f("2b.yaml") )
         with self.assertRaises(Exception):
-            c.run_core()
+            Core(source_file = self.f("2a.yaml"), schema_file = self.f("2b.yaml") ).run_core()
 
         # Test sequence where the only valid items is integers
-        c = Core(source_file = self.f("3a.yaml"), schema_file = self.f("3b.yaml") )
-        c.run_core()
+        Core(source_file = self.f("3a.yaml"), schema_file = self.f("3b.yaml") ).run_core()
 
         # Test sequence with only booleans
-        c = Core(source_file = self.f("4a.yaml"), schema_file = self.f("4b.yaml") )
-        c.run_core()
+        Core(source_file = self.f("4a.yaml"), schema_file = self.f("4b.yaml") ).run_core()
 
         # Test sequence with defined string content type but data only has booleans
-        c = Core(source_file = self.f("5a.yaml"), schema_file = self.f("5b.yaml") )
         with self.assertRaises(Exception):
-            c.run_core()
+            Core(source_file = self.f("5a.yaml"), schema_file = self.f("5b.yaml") ).run_core()
 
         # Test sequence with defined booleans but with one integer
-        c = Core(source_file = self.f("6a.yaml"), schema_file = self.f("6b.yaml") )
         with self.assertRaises(Exception):
-            c.run_core()
+            Core(source_file = self.f("6a.yaml"), schema_file = self.f("6b.yaml") ).run_core()
 
         # Test sequence with strings and and lenght on each string
-        c = Core(source_file = self.f("7a.yaml"), schema_file = self.f("7b.yaml") )
         with self.assertRaises(Exception):
-            c.run_core()
+            Core(source_file = self.f("7a.yaml"), schema_file = self.f("7b.yaml") ).run_core()
 
         # Test mapping with different types of data and some extra conditions
-        c = Core(source_file = self.f("8a.yaml"), schema_file = self.f("8b.yaml") )
-        c.run_core()
+        Core(source_file = self.f("8a.yaml"), schema_file = self.f("8b.yaml") ).run_core()
 
         # Test mapping that do not work
-        c = Core(source_file = self.f("9a.yaml"), schema_file = self.f("8b.yaml") )
         with self.assertRaises(Exception):
-            c.run_core()
+            Core(source_file = self.f("9a.yaml"), schema_file = self.f("8b.yaml") ).run_core()
 
         # Test sequence with mapping with valid mapping
-        c = Core(source_file = self.f("10a.yaml"), schema_file = self.f("10b.yaml") )
-        c.run_core()
+        Core(source_file = self.f("10a.yaml"), schema_file = self.f("10b.yaml") ).run_core()
 
         # Test sequence with mapping with missing required key
-        c = Core(source_file = self.f("11a.yaml"), schema_file = self.f("10b.yaml") )
         with self.assertRaises(Exception):
-            c.run_core()
+            Core(source_file = self.f("11a.yaml"), schema_file = self.f("10b.yaml") ).run_core()
 
         # Test mapping with sequence with mapping and valid data
-        c = Core(source_file = self.f("12a.yaml"), schema_file = self.f("12b.yaml") )
-        c.run_core()
+        Core(source_file = self.f("12a.yaml"), schema_file = self.f("12b.yaml") ).run_core()
 
         # Test mapping with sequence with mapping and invalid data
-        c = Core(source_file = self.f("13a.yaml"), schema_file = self.f("12b.yaml") )
         with self.assertRaises(Exception):
-            c.run_core()
+            Core(source_file = self.f("13a.yaml"), schema_file = self.f("12b.yaml") ).run_core()
 
         # Test most of the implemented functions
-        c = Core(source_file = self.f("14a.yaml"), schema_file = self.f("14b.yaml") )
-        c.run_core()
+        Core(source_file = self.f("14a.yaml"), schema_file = self.f("14b.yaml") ).run_core()
 
-        c = Core(source_file = self.f("15a.yaml"), schema_file = self.f("14b.yaml") )
         with self.assertRaises(Exception):
-            c.run_core()
+            Core(source_file = self.f("15a.yaml"), schema_file = self.f("14b.yaml") ).run_core()
 
         # This will test the unique constraint
-        c = Core(source_file = self.f("16a.yaml"), schema_file = self.f("16b.yaml") )
-        c.run_core()
+        Core(source_file = self.f("16a.yaml"), schema_file = self.f("16b.yaml") ).run_core()
 
         # TODO: The reverse unique do not currently work proper
         # This will test the unique constraint but should fail
-        c = Core(source_file = self.f("17a.yaml"), schema_file = self.f("16b.yaml") )
         with self.assertRaises(Exception):
-            c.run_core()
+            Core(source_file = self.f("17a.yaml"), schema_file = self.f("16b.yaml") ).run_core()
 
-        c = Core(source_file = self.f("18a.yaml"), schema_file = self.f("18b.yaml") )
-        c.run_core()
+        Core(source_file = self.f("18a.yaml"), schema_file = self.f("18b.yaml") ).run_core()
 
-        c = Core(source_file = self.f("19a.yaml"), schema_file = self.f("19b.yaml") )
-        c.run_core()
+        Core(source_file = self.f("19a.yaml"), schema_file = self.f("19b.yaml") ).run_core()
 
-        c = Core(source_file = self.f("20a.yaml"), schema_file = self.f("20b.yaml") )
-        c.run_core()
+        Core(source_file = self.f("20a.yaml"), schema_file = self.f("20b.yaml") ).run_core()
 
         # This tests number validation rule
-        c = Core(source_file = self.f("21a.yaml"), schema_file = self.f("21b.yaml") )
-        c.run_core()
+        Core(source_file = self.f("21a.yaml"), schema_file = self.f("21b.yaml") ).run_core()
 
         # This tests number validation rule with wrong data
-        c = Core(source_file = self.f("22a.yaml"), schema_file = self.f("22b.yaml") )
         with self.assertRaises(Exception):
-            c.run_core()
+            Core(source_file = self.f("22a.yaml"), schema_file = self.f("22b.yaml") ).run_core()
 
         # This test the text validation rule
-        c = Core(source_file = self.f("23a.yaml"), schema_file = self.f("23b.yaml") )
-        c.run_core()
+        Core(source_file = self.f("23a.yaml"), schema_file = self.f("23b.yaml") ).run_core()
 
         # This test the text validation rule with wrong data
-        c = Core(source_file = self.f("24a.yaml"), schema_file = self.f("24b.yaml") )
         with self.assertRaises(Exception):
-            c.run_core()
+            Core(source_file = self.f("24a.yaml"), schema_file = self.f("24b.yaml") ).run_core()
 
         # This test the text validation rule
-        c = Core(source_file = self.f("24a.yaml"), schema_file = self.f("25b.yaml") )
-        c.run_core()
+        Core(source_file = self.f("24a.yaml"), schema_file = self.f("25b.yaml") ).run_core()
 
-        c = Core(source_file = self.f("26a.yaml"), schema_file = self.f("26b.yaml") )
-        c.run_core()
+        Core(source_file = self.f("26a.yaml"), schema_file = self.f("26b.yaml") ).run_core()
 
         # This tests pattern matching on keys in a map
-        c = Core(source_file = self.f("27a.yaml"), schema_file = self.f("27b.yaml") )
         with self.assertRaises(Exception):
-            c.run_core()
+            Core(source_file = self.f("27a.yaml"), schema_file = self.f("27b.yaml") ).run_core()
 
-        c = Core(source_file = self.f("28a.yaml"), schema_file = self.f("28b.yaml") )    
-        c.run_core()
+        Core(source_file = self.f("28a.yaml"), schema_file = self.f("28b.yaml") )    
         
