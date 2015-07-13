8f1c892c1f5579569996512e8ab00dec65e4fcf4
Grokzen <Grokzen@gmail.com>
2015-07-14 00:33:20 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('ReleaseNotes.rst') as f:
 
 setup(
     name="pykwalify",
-    version="1.2.0",
+    version="1.3.0",
     description='Python lib/cli for JSON/YAML schema validation',
     long_description=readme + '\n\n' + history,
     author="Johan Andersson",
