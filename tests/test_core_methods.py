cdf4b3194caa5fb766adc56c492f16bef30dc9db
Grokzen <Grokzen@gmail.com>
2016-03-05 18:28:57 +0100

    Improve error checking in test_validate_sequence and test_validate_scalar_type

--- tests/test_core_methods.py
+++ tests/test_core_methods.py
@@ -12,6 +12,10 @@ class Rule(object):
         self.type = rule_type or ''
 
 
+def _remap_errors(c):
+    return [str(error) for error in c.errors]
+
+
 def test_validate_sequence():
     # If the type is set to sequence but value is int, it should raise NotSequenceError
     with pytest.raises(NotSequenceError):
@@ -27,70 +31,70 @@ def ec():
 def test_validate_timestamp():
     c = ec()
     c._validate_scalar_timestamp("", '')
-    assert len(c.errors) == 1
+    assert _remap_errors(c) == ["Timestamp value is empty. Path: ''"]
 
     c = ec()
     c._validate_scalar_timestamp("1234567", '')
-    assert len(c.errors) == 0
+    assert _remap_errors(c) == []
 
     c = ec()
     c._validate_scalar_timestamp("2016-01-01", '')
-    assert len(c.errors) == 0
+    assert _remap_errors(c) == []
 
     c = ec()
     c._validate_scalar_timestamp("2016-01-01 15:01", '')
-    assert len(c.errors) == 0
+    assert _remap_errors(c) == []
 
     c = ec()
     c._validate_scalar_timestamp(123, '')
-    assert len(c.errors) == 0
+    assert _remap_errors(c) == []
 
     c = ec()
     c._validate_scalar_timestamp(1.5, '')
-    assert len(c.errors) == 0
+    assert _remap_errors(c) == []
 
     c = ec()
     c._validate_scalar_timestamp(0, '')
-    assert len(c.errors) == 1
+    assert _remap_errors(c) == ["Integer value of timestamp can't be below 0"]
 
     c = ec()
     c._validate_scalar_timestamp(-1, '')
-    assert len(c.errors) == 1
+    assert _remap_errors(c) == ["Integer value of timestamp can't be below 0"]
 
     c = ec()
     c._validate_scalar_timestamp(3147483647, '')
-    assert len(c.errors) == 1
+    assert _remap_errors(c) == ["Integer value of timestamp can't be above 2147483647"]
 
     c = ec()
     c._validate_scalar_timestamp([], '')
-    assert len(c.errors) == 1
+    assert _remap_errors(c) == ['Not a valid timestamp']
 
     c = ec()
     c._validate_scalar_timestamp(datetime.now(), '')
-    assert len(c.errors) == 0
+    assert _remap_errors(c) == []
 
     c = ec()
     c._validate_scalar_timestamp(datetime.today(), '')
-    assert len(c.errors) == 0
+    assert _remap_errors(c) == []
 
 
 def test_validate_scalar_type():
     c = ec()
     c._validate_scalar_type("1e-06", "float", '')
-    assert len(c.errors) == 0
+    assert _remap_errors(c) == []
 
     c = ec()
     c._validate_scalar_type("1z-06", "float", '')
-    assert len(c.errors) == 1
+    assert _remap_errors(c) == ["Value '1z-06' is not of type 'float'. Path: ''"]
 
     c = ec()
     c._validate_scalar_type(1.5, "float", '')
-    assert len(c.errors) == 0
+    assert _remap_errors(c) == []
 
     c = ec()
     c._validate_scalar_type("abc", "float", '')
-    assert len(c.errors) == 1
+    assert _remap_errors(c) == ["Value 'abc' is not of type 'float'. Path: ''"]
 
     c = ec()
     c._validate_scalar_type(True, "float", '')
-    assert len(c.errors) == 1
+    assert _remap_errors(c) == ["Value 'True' is not of type 'float'. Path: ''"]
