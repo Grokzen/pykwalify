036f045059aa42ee5c59b9207656ad8f73f730d4
Grokzen <Grokzen@gmail.com>
2016-03-28 13:47:26 +0200

    Remove files with unicode names and replace them with mock files with same content and filenames but only during tests. Filenames with åäö in them for example breaks python dist packaging if they are included in the release, but only happens on some python versions.

--- tests/test_unicode.py
+++ tests/test_unicode.py
@@ -22,23 +22,47 @@ class TestUnicode(object):
         pykwalify.partial_schemas = {}
 
     def f(self, *args):
+        if os.path.isabs(args[0]):
+            return args[0]
+
         return os.path.join(os.path.dirname(os.path.realpath(__file__)), "files", "unicode", *args)
 
-    def test_files_with_unicode_content_success(self):
+    def test_files_with_unicode_content_success(self, tmpdir):
         """
         These tests should pass with no exception raised
         """
+        fail_data_2s_yaml = {
+            'schema': {
+                'type': 'map',
+                'mapping': {
+                    'msg': {
+                        'type': 'int',
+                    },
+                }
+            },
+            'data': {
+                'msg': 123,
+            },
+            'errors': []
+        }
+
+        source_f = tmpdir.join(u"2så.json")
+        source_f.write(yaml.safe_dump(fail_data_2s_yaml, allow_unicode=True))
+
         _pass_tests = [
             # Test mapping with unicode key and value
             u"1s.yaml",
-            # Test unicode filename
-            u"2så.yaml",
+            # # Test unicode filename.
+            # It is not possible to package a file with unicode characters
+            # like åäö in the filename in some python versions.
+            # Mock a file with åäö during testing to properly simulate this again.
+            unicode(source_f),
             # Test sequence with unicode keys
             u"3s.yaml",
         ]
 
         for passing_test_files in _pass_tests:
-            f = unicode(self.f(passing_test_files))
+            f = self.f(passing_test_files)
 
             with open(f, "r") as stream:
                 yaml_data = yaml.load(stream)
@@ -57,22 +81,43 @@ class TestUnicode(object):
             # This serve as an extra schema validation that tests more complex structures then testrule.py do
             compare(c.root_rule.schema_str, schema, prefix=u"Parsed rules is not correct, something have changed... files : {}".format(f))
 
-    def test_files_with_unicode_content_failing(self):
+    def test_files_with_unicode_content_failing(self, tmpdir):
         """
         These tests should fail with the specified exception
         """
+        # To trigger schema exception we must pass in a source file
+        fail_data_2f_yaml = {
+            'schema': {
+                'type': 'map',
+                'mapping': {
+                    'msg': {
+                        'type': 'int',
+                    },
+                }
+            },
+            'data': {
+                'msg': 'Foobar',
+            },
+            'errors': ["Value 'Foobar' is not of type 'int'. Path: '/msg'"]
+        }
+
+        source_f = tmpdir.join(u"2få.json")
+        source_f.write(yaml.safe_dump(fail_data_2f_yaml, allow_unicode=True))
+
         _fail_tests = [
             # Test mapping with unicode key and value but wrong type
             (u"1f.yaml", SchemaError),
-            # Test unicode filename with validation errors
-            (u"2få.yaml", SchemaError),
+            # Test unicode filename with validation errors.
+            # It is not possible to package a file with unicode characters
+            # like åäö in the filename in some python versions.
+            # Mock a file with åäö during testing to properly simulate this again.
+            (unicode(source_f), SchemaError),
             # Test unicode data inside seq but wrong type
             (u"3f.yaml", SchemaError),
         ]
 
         for failing_test, exception_type in _fail_tests:
-            # f = self.f(os.path.join("fail", failing_test))
-            f = unicode(self.f(failing_test))
+            f = self.f(failing_test)
 
             with open(f, "r") as stream:
                 yaml_data = yaml.load(stream)
