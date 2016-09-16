a5667b6a6b5f1c86beb6ed8307315e32c59b4aaf
Grokzen <Grokzen@gmail.com>
2016-09-16 09:22:01 +0200

    Prepare for release 1.5.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join('docs', 'release-notes.rst')) as f:
 
 setup(
     name="pykwalify",
-    version="1.5.1",
+    version="1.5.2",
     description='Python lib/cli for JSON/YAML schema validation',
     long_description=readme + '\n\n' + history,
     author="Johan Andersson",
